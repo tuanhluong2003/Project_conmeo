b673510695e81b028c971f1ee505297c4350ba83
Tu Anh Luong <luongtu180103@gmail.com>
2023-09-17 13:45:41 +0700

    house price

---
+++ house_price.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+tmpx = []
+tmpy = []
+index = 1
+with open('input.txt') as rf:
+    line = rf.readline()
+    while line:
+        tmpline = [float(i) for i in line.split()]
+        tmpx.append(tmpline[:-1])
+        tmpy.append([tmpline[-1]])
+        index += 1
+        line = rf.readline()
+
+
+X_train = np.array(tmpx).T
+Y_train = np.array(tmpy)
+
+w = np.linalg.pinv(X_train@X_train.T)@X_train@Y_train
+print(X_train.T@w)
