f52389a40c72f0566fa99eda8015f1c96d0fde64
Tu Anh Luong <luongtu180103@gmail.com>
2023-09-19 01:02:40 +0700

    update lan cuoi

--- house_price.py
+++ house_price.py
@@ -1,19 +1,19 @@
 import numpy as np
+import pandas as pd
 
-tmpx = []
-tmpy = []
-index = 1
-with open('input.txt') as rf:
-    line = rf.readline()
-    while line:
-        tmpline = [float(i) for i in line.split()]
-        tmpx.append(tmpline[:-1])
-        tmpy.append([tmpline[-1]])
-        index += 1
-        line = rf.readline()
-X = np.array(tmpx)
-X_train = np.insert(X,0,1,axis = 1).T
-Y_train = np.array(tmpy)
+#doc du lieu tu file
+data = pd.read_csv('USA_Housing.csv',header=None)
+X_train = np.array([data[0], data[1], data[2], data[3], data[4]])
 
+#chen mot cot mot vao hang dau tien
+X_train = np.insert(X_train,0,1,axis = 0)
+Y_train = np.array([data[5]]).T
+
+
+
+X_test = np.array([1, 79545, 5, 7, 4, 23086])
 w = np.linalg.pinv(X_train@X_train.T)@X_train@Y_train
+print("w = ", w)
+print('Giá trị dự đoán mẫu mới: ',X_test@w)
+print("Giá trị dự đoán tập huấn luyện:")
 print(X_train.T@w)
--- house_price_sklearn.py
+++ house_price_sklearn.py
@@ -1,1 +1,22 @@
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+#doc du lieu tu file
+data = pd.read_csv('USA_Housing.csv',header=None)
+X_train = np.array([data[0], data[1], data[2], data[3], data[4]]).T
+#chen tu dong mot cot 1 vao cot dau tien
+X_train = np.insert(X_train,0,1,axis = 1)
+Y_train = np.array([data[5]]).T
+
+reg = LinearRegression().fit(X_train,Y_train)
+
+x_test = np.array([[1,79545, 5, 7, 4, 23086]])
+
+print("w = ", reg.coef_)
+# Sai số
+print('w0 = ',reg.intercept_)
+print('Giá trị dự đoán mẫu mới: ',reg.predict(x_test))
+print("Giá trị dự đoán tập huấn luyện:")
+print(reg.predict(X_train))
